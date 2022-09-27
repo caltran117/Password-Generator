e557370d6a3ef7b0ca75edabd25eb2d1d62e4315
Calvin <84696281+caltran117@users.noreply.github.com>
2022-09-26 23:41:39 -0400

    adding a prompt for pw type & length, removed old code that only had random password w/out prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// <!-- WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters -->
+prompt("Choose a length of at least 8 characters and no more than 128 characters" )
+
+// <!-- WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters -->
+prompt("Enter type: lowercase, uppercase, numeric, and/or special characters")
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,10 +33,23 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+var password_length = Number(prompt("Choose a length of at least 8 characters and no more than 128 characters"));
+var type = prompt("Enter type: lowercase, uppercase, numeric, and/or special characters")
+
 function generatePassword(){
-  var newpassword= ""
-  var character_list="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()"
-  for (let i = 0; i < 10; i++) {
+  var newpassword = "";
+ if(newpassword === "lowercase") {
+  newpassword = "qwertyuiopasdfghjklzxcvbnm";
+} else if (newpassword === "uppercase") {
+  newpassword = "QWERTYUIOPASDFGHJKLZXCVBNM";
+} else if (newpassword === "numerical") {
+  newpassword = "1234567890";
+} else if (newpassword === "special") {
+  newpassword = "!@#$%^&*";
+}
+
+  for (let i = 0; i < password_length; i++) {
     newpassword+=character_list.charAt(Math.floor(Math.random()*character_list.length))
   }
   return newpassword
